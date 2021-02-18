9d870d06742d003ba033e3b0234cb9602cc04f34
SebastianZug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-02-18 16:02:12 +0100

    feat: Fügt HelloWorld cmsis Beispiel ein
    Siehe: #1

---
+++ codeExamples/STM32/HelloWorld/cmsis/src/main.c
@@ -0,0 +1,41 @@
+#include "stm32f4xx.h"
+#include <stdbool.h>
+
+void ms_delay(int ms)
+{
+   while (ms-- > 0) {
+      volatile int x=500;
+      while (x-- > 0)
+         __asm("nop");
+   }
+}
+
+// Version 1: Basic Register Usage
+
+// https://www.vishnumaiea.in/articles/electronics/understanding-stm32-arm-microcontroller-gpios
+
+int main(void){
+   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+   GPIOA->MODER |= GPIO_MODER_MODER5_0; 
+   while(true){
+      ms_delay(500);
+      GPIOA->ODR ^= GPIO_BSRR_BS5;
+   }
+}
+
+// Version 2: STM Macros for Bit manipulation
+
+//int main(void)
+//{
+//   SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+//   MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODER5,  GPIO_MODER_MODER5_0);
+//
+//   while(true){
+//      ms_delay(500);
+//      WRITE_REG(GPIOA->BSRR, GPIO_BSRR_BS_5);
+//      ms_delay(500);
+//      WRITE_REG(GPIOA->BSRR, GPIO_BSRR_BR_5);
+//   }
+//
+//   return 0;
+//}
