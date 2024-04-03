ed097f8487bcb4ef98faaf45fb89722c17ccd316
Sebastian Zug <sebastian.zug@informatik.tu-freiberg.de>
2024-04-03 15:21:03 +0200

    Integrate asm files

---
+++ codeExamples/avr/helloWorld_C/main.s
@@ -0,0 +1,79 @@
+	.file	"main.c"
+__SP_H__ = 0x3e
+__SP_L__ = 0x3d
+__SREG__ = 0x3f
+__tmp_reg__ = 0
+__zero_reg__ = 1
+ ;  GNU C11 (GCC) version 5.4.0 (avr)
+ ; 	compiled by GNU C version 11.2.0, GMP version 6.2.1, MPFR version 4.1.0, MPC version 1.2.1
+ ;  GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+ ;  options passed:  -fpreprocessed main.i -mn-flash=1 -mno-skip-bug
+ ;  -mno-interrupts -mmcu=avr5 -auxbase-strip main.o -Os -fverbose-asm
+ ;  options enabled:  -Wmisspelled-isr -faggressive-loop-optimizations
+ ;  -falign-functions -falign-jumps -falign-labels -falign-loops
+ ;  -fauto-inc-dec -fbranch-count-reg -fchkp-check-incomplete-type
+ ;  -fchkp-check-read -fchkp-check-write -fchkp-instrument-calls
+ ;  -fchkp-narrow-bounds -fchkp-optimize -fchkp-store-bounds
+ ;  -fchkp-use-static-bounds -fchkp-use-static-const-bounds
+ ;  -fchkp-use-wrappers -fcombine-stack-adjustments -fcommon -fcompare-elim
+ ;  -fcprop-registers -fcrossjumping -fcse-follow-jumps -fdefer-pop
+ ;  -fdevirtualize -fdevirtualize-speculatively -fdwarf2-cfi-asm
+ ;  -fearly-inlining -feliminate-unused-debug-types
+ ;  -fexpensive-optimizations -fforward-propagate -ffunction-cse -fgcse
+ ;  -fgcse-lm -fgnu-runtime -fgnu-unique -fguess-branch-probability
+ ;  -fhoist-adjacent-loads -fident -fif-conversion -fif-conversion2
+ ;  -findirect-inlining -finline -finline-atomics -finline-functions
+ ;  -finline-functions-called-once -finline-small-functions -fipa-cp
+ ;  -fipa-cp-alignment -fipa-icf -fipa-icf-functions -fipa-icf-variables
+ ;  -fipa-profile -fipa-pure-const -fipa-ra -fipa-reference -fipa-sra
+ ;  -fira-hoist-pressure -fira-share-save-slots -fira-share-spill-slots
+ ;  -fisolate-erroneous-paths-dereference -fivopts -fkeep-static-consts
+ ;  -fleading-underscore -flifetime-dse -flra-remat -flto-odr-type-merging
+ ;  -fmath-errno -fmerge-constants -fmerge-debug-strings
+ ;  -fmove-loop-invariants -fomit-frame-pointer -foptimize-sibling-calls
+ ;  -fpartial-inlining -fpeephole -fpeephole2 -fprefetch-loop-arrays
+ ;  -freg-struct-return -freorder-blocks -freorder-functions
+ ;  -frerun-cse-after-loop -fsched-critical-path-heuristic
+ ;  -fsched-dep-count-heuristic -fsched-group-heuristic -fsched-interblock
+ ;  -fsched-last-insn-heuristic -fsched-rank-heuristic -fsched-spec
+ ;  -fsched-spec-insn-heuristic -fsched-stalled-insns-dep -fschedule-fusion
+ ;  -fsemantic-interposition -fshow-column -fshrink-wrap -fsigned-zeros
+ ;  -fsplit-ivs-in-unroller -fsplit-wide-types -fssa-phiopt -fstdarg-opt
+ ;  -fstrict-aliasing -fstrict-overflow -fstrict-volatile-bitfields
+ ;  -fsync-libcalls -fthread-jumps -ftoplevel-reorder -ftrapping-math
+ ;  -ftree-bit-ccp -ftree-builtin-call-dce -ftree-ccp -ftree-ch
+ ;  -ftree-coalesce-vars -ftree-copy-prop -ftree-copyrename -ftree-dce
+ ;  -ftree-dominator-opts -ftree-dse -ftree-forwprop -ftree-fre
+ ;  -ftree-loop-if-convert -ftree-loop-im -ftree-loop-ivcanon
+ ;  -ftree-loop-optimize -ftree-parallelize-loops= -ftree-phiprop
+ ;  -ftree-pre -ftree-pta -ftree-reassoc -ftree-scev-cprop -ftree-sink
+ ;  -ftree-slsr -ftree-sra -ftree-switch-conversion -ftree-tail-merge
+ ;  -ftree-ter -ftree-vrp -funit-at-a-time -fverbose-asm
+ ;  -fzero-initialized-in-bss -mno-interrupts
+
+	.section	.text.startup,"ax",@progbits
+.global	main
+	.type	main, @function
+main:
+/* prologue: function */
+/* frame size = 0 */
+/* stack size = 0 */
+.L__stack_usage = 0
+	sbi 0x4,0	 ; ,
+	ldi r25,lo8(1)	 ;  tmp52,
+.L2:
+	in r24,0x5	 ;  D.1752, MEM[(volatile uint8_t *)37B]
+	eor r24,r25	 ;  D.1752, tmp52
+	out 0x5,r24	 ;  MEM[(volatile uint8_t *)37B], D.1752
+	ldi r18,lo8(812799)	 ; ,
+	ldi r19,hi8(812799)	 ; ,
+	ldi r24,hlo8(812799)	 ; ,
+1:	subi r18,1	 ; 
+	sbci r19,0	 ; 
+	sbci r24,0	 ; 
+	brne 1b
+	rjmp .
+	nop
+	rjmp .L2	 ; 
+	.size	main, .-main
+	.ident	"GCC: (GNU) 5.4.0"
